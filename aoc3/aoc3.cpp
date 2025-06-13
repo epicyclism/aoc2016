e5b07255e6dfd9d92991bf38ba1005596a50e2cf
Paul <paul@epicyclism.com>
2025-06-13 22:14:58 +0100

    aoc3 1

--- aoc3/aoc3.cpp
+++ aoc3/aoc3.cpp
@@ -1,26 +1,54 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <array>
 #include <algorithm>
 #include <numeric>
 #include <ranges>
 
 #include <fmt/format.h>
 
-//#include "ctre.hpp"
+#include "ctre.hpp"
 #include "timer.h"
+
+using tri_t = std::array<int, 3>;
+
+void sort_tri(tri_t& tt)
+{
+	if(tt[0] > tt[1])
+		std::swap(tt[0], tt[1]);
+	if(tt[1] > tt[2])
+		std::swap(tt[1], tt[2]);
+	if(tt[0] > tt[1])
+		std::swap(tt[0], tt[1]);
+}
+
+bool tri_good(tri_t tt)
+{
+	sort_tri(tt);
+	return tt[2] < tt[0] + tt[1];
+}
 
 auto get_input()
 {
-	return 0;
+	std::vector<tri_t> r;
+	std::string ln;
+	while(std::getline(std::cin, ln))
+		if(auto[m, a, b, c] = ctre::match<"[ ]+(\\d+)[ ]+(\\d+)[ ]+(\\d+)">(ln); m)
+		{
+			tri_t tt{a.to_number<int>(), b.to_number<int>(), c.to_number<int>()};
+			r.emplace_back(tt);
+		}
+	return r;
 }
 
-int64_t pt1(auto const& in_addr_t)
+int pt1(auto const& in)
 {
 	timer t("p1");
-	return 0;
+	return std::ranges::count_if(in, tri_good);
 }
 
-int64_t pt2(auto const& in)
+int pt2(auto const& in)
 {
 	timer t("p2");
 	return 0;
