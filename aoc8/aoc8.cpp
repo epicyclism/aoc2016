e1cea2eb2d87d927de8539990f66d361d49fab8e
Paul <paul@epicyclism.com>
2025-06-15 18:29:20 +0100

    aoc8

--- aoc8/aoc8.cpp
+++ aoc8/aoc8.cpp
@@ -1,36 +1,121 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <array>
 #include <algorithm>
 #include <numeric>
 #include <ranges>
 
 #include <fmt/format.h>
 
-//#include "ctre.hpp"
+#include "ctre.hpp"
 #include "timer.h"
+
+enum class cmd_v {set, r_row, r_col};
+
+struct cmd_t
+{
+	cmd_v cmd_;
+	int x_;
+	int y_;
+};
 
 auto get_input()
 {
-	return 0;
+	std::vector<cmd_t> v;
+	std::string ln;
+	while(std::getline(std::cin, ln))
+	{
+		if(auto[m, x, y] = ctre::match<"rect (\\d+)x(\\d+)">(ln); m)
+			v.emplace_back(cmd_v::set, x.to_number<int>(), y.to_number<int>());
+		else
+		if(auto[m, x, y] = ctre::match<"rotate row y=(\\d+) by (\\d+)">(ln); m)
+			v.emplace_back(cmd_v::r_row, x.to_number<int>(), y.to_number<int>());
+		else
+		if(auto[m, x, y] = ctre::match<"rotate column x=(\\d+) by (\\d+)">(ln); m)
+			v.emplace_back(cmd_v::r_col, x.to_number<int>(), y.to_number<int>());
+		else
+			fmt::println("parse fail : {}", ln);
+	}
+	return v;
 }
 
-int64_t pt1(auto const& in_addr_t)
+constexpr int WD = 50;
+constexpr int HT = 6; 
+
+void print(std::array<char, WD*HT> const& scr)
 {
-	timer t("p1");
-	return 0;
+	for(auto r = 0; r < HT; ++r)
+	{
+		std::string_view v(scr.begin() + r * WD, scr.begin() + (r + 1) * WD);
+		fmt::println("{}", v);
+	}
 }
 
-int64_t pt2(auto const& in)
+void proc(std::vector<cmd_t> const& v, std::array<char, WD*HT>& scr)
 {
-	timer t("p2");
-	return 0;
+	std::array<char, HT> tmp_col;
+	auto copy_col_out = [&](int col)
+	{
+		auto itf = std::ranges::begin(scr) + col;
+		auto itt = std::ranges::begin(tmp_col);
+		for(int n = 0; n < HT; ++n)
+		{
+			*itt = *itf;
+			++itt;
+			itf += WD;
+		}
+	};
+	auto copy_col_in  = [&](int col)
+	{
+		auto itt = std::ranges::begin(scr) + col;
+		auto itf = std::ranges::begin(tmp_col);
+		for(int n = 0; n < HT; ++n)
+		{
+			*itt = *itf;
+			itt += WD;
+			++itf;
+		}
+	};
+	for(auto& c: v)
+	{
+		auto itb = std::ranges::begin(scr);
+		switch(c.cmd_)
+		{
+			case cmd_v::set:
+				for(int y = 0; y < c.y_; ++y)
+				{
+					for(auto itw = itb; itw != itb + c.x_; ++itw)
+						*itw = '#';
+					itb += WD;
+				}
+				break;
+			case cmd_v::r_row:
+				itb += c.x_ * WD;
+				std::ranges::rotate(itb, itb + (WD - c.y_), itb + WD );
+				break;
+			case cmd_v::r_col:
+				copy_col_out(c.x_);
+				std::ranges::rotate(std::ranges::begin(tmp_col), std::ranges::begin(tmp_col) + (HT - c.y_), std::ranges::end(tmp_col) );
+				copy_col_in(c.x_);
+				break;
+		}
+	}
+}
+
+auto pt12(auto const& in)
+{
+	timer t("p12");
+	std::array<char, WD * HT> scr;
+	scr.fill('.');
+	proc(in, scr);
+	return std::make_pair(std::ranges::count(scr, '#'), scr);
 }
 
 int main()
 {
 	auto in = get_input();
-	auto p1 = pt1(in);
-	auto p2 = pt2(in);
+	auto[ p1, p2] = pt12(in);
 	fmt::println("pt1 = {}", p1);
-	fmt::println("pt2 = {}", p2);
+	print(p2);
 }
