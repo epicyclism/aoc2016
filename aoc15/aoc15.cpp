e4081fc3838cd79f8f4e30aa510b96e78d24f880
Paul <paul@epicyclism.com>
2025-07-01 19:39:27 +0100

    aoc15

--- aoc15/aoc15.cpp
+++ aoc15/aoc15.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <algorithm>
 #include <numeric>
 #include <ranges>
@@ -11,26 +12,49 @@
 
 auto get_input()
 {
-	return 0;
+	std::vector<std::pair<unsigned, unsigned>> v;
+	std::string ln;
+	int t = 1;
+	while(std::getline(std::cin, ln))
+	{
+		if(auto[m, th, i] = ctre::match<"Disc #\\d has (\\d+) positions; at time=0, it is at position (\\d+).">(ln); m)
+		{
+			auto teeth = th.to_number<unsigned>();
+			auto init = i.to_number<unsigned>();
+			init += t;
+			init %= teeth;
+			init = teeth - init;
+			init %= teeth;
+			v.emplace_back(teeth, init);
+		}
+		else
+			fmt::println("parse fail at {}", ln);
+		++t;
+	}
+	std::ranges::sort(v, [](auto& l, auto& r){ return l.first > r.first;});
+	return v;
 }
 
-int64_t pt1(auto const& in_addr_t)
+int pt12(auto const& in)
 {
-	timer t("p1");
-	return 0;
-}
-
-int64_t pt2(auto const& in)
-{
-	timer t("p2");
-	return 0;
+	timer t("p12");
+	int time = in[0].second;
+	while(1)
+	{
+		if(std::ranges::all_of(in, [=](auto& p){ return (time % p.first) == p.second;}))
+			break;
+		time += in[0].first;
+	}
+	return time;
 }
 
 int main()
 {
 	auto in = get_input();
-	auto p1 = pt1(in);
-	auto p2 = pt2(in);
+	auto p1 = pt12(in);
+	unsigned d11 = (11 - in.size() - 1) % 11;
+	in.emplace_back(11, d11);
+	auto p2 = pt12(in);
 	fmt::println("pt1 = {}", p1);
 	fmt::println("pt2 = {}", p2);
 }
