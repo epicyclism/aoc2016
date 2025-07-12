7ee1deef69f7973f9c6f364f8bee3cd21def4b73
Paul <paul@epicyclism.com>
2025-07-12 13:59:22 +0100

    aoc18

--- aoc18/aoc18.cpp
+++ aoc18/aoc18.cpp
@@ -6,24 +6,67 @@
 
 #include <fmt/format.h>
 
-#include "ctre_inc.h"
 #include "timer.h"
 
 auto get_input()
 {
-	return 0;
+	std::string ln;
+	std::getline(std::cin, ln);
+	return ln;
 }
 
-int64_t pt1(auto const& in_addr_t)
+std::string next_row(std::string_view rw)
+{
+	std::string r;
+	r.resize(rw.size());
+	char lft = '.';
+	char rgt = '.';
+	for(int n = 0; n < rw.size(); ++n)
+	{
+		if( n > 0)
+			lft = rw[n - 1];
+		if( n < rw.size() - 1)
+			rgt = rw[n + 1];
+		else
+			rgt = '.';
+		auto cntr = rw[n];
+		if(lft == '^' && rgt == '.')
+			r[n] = '^';
+		else
+		if(lft == '.' && rgt == '^')
+			r[n] = '^';
+		else
+			r[n] = '.';
+	}
+	return r;
+}
+
+int pt1(auto const& in)
 {
 	timer t("p1");
-	return 0;
+	auto s = in;
+	int cnt = 0;
+	int lim = in.size() < 40 ? in.size() : 40;
+	for(int n = 0; n < lim; ++n)
+	{
+		cnt += std::ranges::count(s.begin(), s.end(), '.');
+//		fmt::println("{}", s);
+		s = next_row(s);
+	}
+	return cnt;
 }
 
 int64_t pt2(auto const& in)
 {
 	timer t("p2");
-	return 0;
+	auto s = in;
+	int cnt = 0;
+	for(int n = 0; n < 400000; ++n)
+	{
+		cnt += std::ranges::count(s.begin(), s.end(), '.');
+		s = next_row(s);
+	}
+	return cnt;
 }
 
 int main()
--- aoc19/aoc19.cpp
+++ aoc19/aoc19.cpp
@@ -9,26 +9,23 @@
 #include "ctre_inc.h"
 #include "timer.h"
 
-auto get_input()
-{
-	return 0;
-}
-
-int64_t pt1(auto const& in_addr_t)
+int pt1(auto in)
 {
 	timer t("p1");
 	return 0;
 }
 
-int64_t pt2(auto const& in)
+int pt2(auto in)
 {
 	timer t("p2");
 	return 0;
 }
 
-int main()
+int main(int ac, char** av)
 {
-	auto in = get_input();
+	auto in = 3014387;
+	if( ac > 1)
+		in = sv_to_t<int>(av[1]);
 	auto p1 = pt1(in);
 	auto p2 = pt2(in);
 	fmt::println("pt1 = {}", p1);
