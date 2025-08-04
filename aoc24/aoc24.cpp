3dd1a2a1140d45d6f012de50f9fc4c8cb1c0a9a8
Paul <paul@epicyclism.com>
2025-08-04 17:57:21 +0100

    graph grid with border...

--- aoc24/aoc24.cpp
+++ aoc24/aoc24.cpp
@@ -83,7 +83,7 @@ auto pt12(auto const& in)
 {
 	timer t("p1");
 	std::vector<std::vector<int>> al(in.pts_.size());
-	grid_direct g (in.gd_, in.st_, [&](auto f, auto t){return in.gd_[t] != '#';}); 
+	grid_direct_border g (in.gd_, in.st_, [&](aauto t){return g.value(t) != '#';}); 
 	for(int spv = 0; spv < in.pts_.size(); ++spv)
 	{
 		auto d = bfs(g, in.pts_[spv]);
